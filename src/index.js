40207ee97280fde64ca1deab6a6e465ce16da112
Ernest <143043338+e05910879@users.noreply.github.com>
2023-11-08 13:09:24 -0800

    Refactor to create barebones homepage from js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello from src/index.js');
+// console.log('Hello from src/index.js');
+
+const body = document.querySelector('body');
+body.setAttribute('style', 'font-style: italic; text-align: center; background-color: lightyellow;');
+
+
+const content = document.querySelector('div#content');
+
+// Create and append headline
+const headline = document.createElement('h1');
+headline.textContent = 'La Restaurante';
+content.appendChild(headline);
+
+// Create and append image
+const image = document.createElement('img');
+image.setAttribute('src', '../src/images/restaurant-image');
+image.setAttribute('alt', 'People laughing at dinner table.');
+image.setAttribute('width', '600px');
+image.setAttribute('height', 'auto');
+content.appendChild(image);
+
+// Create and append copy
+const copy = document.createElement('h2');
+copy.textContent = '"Where food and happiness come together."';
+content.appendChild(copy);
+
